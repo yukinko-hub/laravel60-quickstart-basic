8f54fbd5c51bc4694b0d4139cb173ed63e10ca8a
yukinko <snow.yk.7385gm@gmail.com>
2020-01-01 21:09:14 +0900

    create task src folder

---
+++ src/routes/web.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+use App\Task;
+use Illuminate\Http\Request;
+
+/**
+ * タスクダッシュボード表示
+ */
+Route::get('/', function () {
+    $tasks = Task::orderBy('created_at','asc')->get();
+
+    return view('tasks',[
+        'tasks' => $tasks
+    ]);
+});
+
+/**
+ * 新タスク追加
+ */
+Route::post('/task',function(Request $request){
+    $validator =Validator::make($request->all(),[
+        'name' => 'required|max:255',
+    ]);
+
+    if($validator->fails()){
+        return redirect('/')
+        ->withInput()
+        ->withErrors($validator);
+    }
+
+    // タスク作成処理
+    $task = new Task();
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+});
+
+/**
+ * タスク削除
+ */
+Route::delete('/task/{task}',function (Task $task){
+    $task->delete();
+
+    return redirect('/');
+});
